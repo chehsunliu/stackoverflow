52985aad7602f94bae557d78ac9593fcc8adadff
chehsunliu <chehsunliu@gmail.com>
2021-08-03 13:15:50 +0800

    q56332118

---
+++ q56332118-multiple-dependency-versions-with-gradle-5/aa1/src/main/java/com/github/chehsunliu/stackoverflow/q56332118/App.java
@@ -0,0 +1,10 @@
+package com.github.chehsunliu.stackoverflow.q56332118;
+
+import org.elasticsearch.spark.cfg.SparkSettings;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println(SparkSettings.class);
+    System.out.println(org.elasticsearch_v6_2_2.spark.cfg.SparkSettings.class);
+  }
+}
